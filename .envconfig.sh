f84ab0e10cac54c03cf7b59e9cd70b045da59a73
Jelani Woods <jelani@firstdraft.com>
2020-09-11 14:57:44 -0500

    Test simpler version

---
+++ .envconfig.sh
@@ -0,0 +1,19 @@
+# install dependencies
+
+echo Initializing...
+
+#Create directories to store persistent data
+mkdir -p /workspace/conda
+mkdir -p /workspace/data
+
+#Create a new env called arcw
+conda create --prefix /workspace/conda/arcw python=3.6 &&
+echo "conda activate /workspace/conda/arcw" >> ~/.bashrc &&
+export PATH=/workspace/conda/arcw/bin:$PATH &&
+source ~/.bashrc
+export SHELL=/bin/bash
+
+#Install required packages specified in requirements.txt
+conda install -r requirements.txt
+
+echo Done...
